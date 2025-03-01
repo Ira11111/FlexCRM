ef994ab45d21db1ee58575075d9dd4b4b9b33a03
senya <zernovasenia@gmail.com>
2025-03-01 16:45:58 +0300

    update auth components

--- frontend/FlexCRM/src/pages/myAuth/Register.tsx
+++ frontend/FlexCRM/src/pages/myAuth/Register.tsx
@@ -1,9 +1,75 @@
+import React, {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import Loader from "../../components/Loader/Loader.tsx";
+import Svglogo from "../../assets/crmLogo.svg";
 import './auth.css'
 
 
 function Register() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [password, setPassword] = useState('');
+    const [cpassword, setcPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (password !== cpassword) {
+            alert('Пароли не совпадают!')
+        }
+        else {
+            setLoading(true);
+            try {
+
+                navigate('/statistic')
+            } catch (e) {
+                console.log(e)
+            } finally {
+                setLoading(false);
+            }
+        }
+    }
     return (
-        <div></div>
+        <div className='auth'>
+            <Link className='auth-form__link' to='/'><img className='auth-form__logo' src={Svglogo} alt={'Лого'}/></Link>
+            <div className='auth-form__wrapper'>
+                <form className='auth-form' onSubmit={handleSubmit} method="post">
+                    <h1 className='auth-form__title'>Регистрация</h1>
+                    {/*<label hidden={true} htmlFor='firstName'>Имя</label>*/}
+                    {/*<input className='auth-form__input' id='firstName' type='text' name='first_name' placeholder='Введите имя'*/}
+                    {/*       onChange={(e)=> setFirstname(e.target.value)}/>*/}
+
+                    {/*<label hidden={true} htmlFor='lastName'>Фамилия</label>*/}
+                    {/*<input className='auth-form__input' id='lastName' type='text' name='last_name' placeholder='Введите фамилию'*/}
+                    {/*       onChange={(e)=> setLastname(e.target.value)}/>*/}
+
+                    <label hidden={true} htmlFor='username'>Имя пользователя</label>
+                    <input className='auth-form__input' required={true} id='username' type='text'  placeholder='Введите имя пользователя'
+                           onChange={(e)=> setUsername(e.target.value)}/>
+
+                    <label hidden={true} htmlFor='email'>Электронная почта</label>
+                    <input className='auth-form__input' required={true} id='email' type='email' name='email' placeholder='Введите электронную почту'
+                           onChange={(e)=> setEmail(e.target.value)}/>
+
+                    <label hidden={true} htmlFor="password">Пароль</label>
+                    <input className='auth-form__input' required minLength={8} id={"password"} placeholder='Введите пароль' type='password' value={password}
+                           onChange={(e)=> setPassword(e.target.value)}/>
+
+                    <label hidden={true} htmlFor="cpassword">Повторите пароль</label>
+                    <input className='auth-form__input' required minLength={8} id={"cpassword"} placeholder='Введите пароль еще раз' type='password' value={cpassword}
+                           onChange={(e)=> setcPassword(e.target.value)}/>
+                    {cpassword!==''  && cpassword !== password && <p>Пароли не совпадают</p>}
+                    <button className='auth-form__button button' type="submit">Зарегистрироваться</button>
+                </form>
+                <p className='auth-form__descr'>Уже есть аккаунт? <Link className='auth-form__link' to='/login'>Войти</Link></p>
+            </div>
+            {loading && <Loader/>}
+
+        </div>
     );
 }
 
