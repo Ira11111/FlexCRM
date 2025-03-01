580687fa227ede225d1be1c166a38375792ce30d
Ira11111 <mirina-0208@yandex.ru>
2025-03-01 14:58:19 +0300

    token views added

--- backend/FlexCRM/crmauth/urls.py
+++ backend/FlexCRM/crmauth/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import CreateUserView
 
 app_name = "crmauth"
 
 urlpatterns = [
     path('register/', CreateUserView.as_view(), name="register"),
+    path('token/obtain/', TokenObtainPairView.as_view(), name="token_obtain"),
+    path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/', include('rest_framework.urls')),
 ]
