8e77718ab32648be1336095d4fb7936f048f0121
Ira11111 <mirina-0208@yandex.ru>
2025-03-08 20:21:52 +0300

    register and add_user working ura!(novernoe)

--- backend/FlexCRM/crmauth/serializers.py
+++ backend/FlexCRM/crmauth/serializers.py
@@ -1,9 +1,16 @@
-from django.contrib.auth.models import User
-from rest_framework.serializers import ModelSerializer, ValidationError
+from django.contrib.auth.models import User, Group, Permission
+from rest_framework.serializers import ModelSerializer, ValidationError, CharField
+
+GROUP_PERMS = {
+    "Admins": ("add_user", "change_user", "delete_user", "view_user"),
+    "Operators": ("add_customer", "change_customer", "view_customer"),
+    "Managers": ("add_contract", "change_contract", "view_contract"),
+    "Marketers": ("add_add", "change_add", "view_add", "change_product", "add_product", "view_product"),
+}
 
 
-def check_group_name(group: str):
-    if group not in ("Operators", "Managers", "Marketers", "Admins"):
+def check_group_name(group_name: str):
+    if group_name not in GROUP_PERMS.keys():
         raise ValidationError("There is no such group")
 
 
@@ -13,17 +20,27 @@ def check_email_in_system(email: str):
 
 
 class UserSerializer(ModelSerializer):
+    user_group = CharField(write_only=True, required=True, validators=[check_group_name])
+
     class Meta:
         model = User
-        fields = ["pk", "username", "password", "email"]
+        fields = ["pk", "username", "password", "email", "user_group"]
         extra_kwargs = {
             "email": {
                 "required": True,
                 "validators": [check_email_in_system]
             },
             "password": {"write_only": True},
-            "user_group": {
-                "required": True,
-                "validators": [check_group_name]
-            }
         }
+
+    def create(self, validated_data):
+        group_name = validated_data.pop("user_group")
+        group, created = Group.objects.get_or_create(name=group_name)
+        for p in GROUP_PERMS.get(group_name):
+            permission = Permission.objects.get(codename=p)
+            group.permissions.add(permission)
+
+        user = User.objects.create_user(**validated_data)
+        user.groups.add(group)
+        user.save()
+        return user
--- backend/FlexCRM/crmauth/views.py
+++ backend/FlexCRM/crmauth/views.py
@@ -1,40 +1,14 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework import generics
 from .serializers import UserSerializer
-from django.contrib.auth.models import User, Group, Permission
-
-GROUP_PERMS = {
-    "Admins": ("add_user", "change_user", "delete_user", "view_user"),
-    "Operators": ("add_customer", "change_customer", "view_customer"),
-    "Managers": ("add_contract", "change_contract", "view_contract"),
-    "Marketers": ("add_add", "change_add", "view_add", "change_product", "add_product", "view_product"),
-}
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 
 
-class CreateUserView(generics.CreateAPIView):
+class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if not serializer.is_valid(raise_exception=True):
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        new_user = serializer.save()
-        user_group_name = request.data.get("user_group")
-
-        if self.request.user.is_authenticated:
-            if not request.user.groups.filter(name="Admins").exists():
-                return Response({"detail": "Only admins can add new users"}, status=status.HTTP_403_FORBIDDEN)
-
-        user_group, created = Group.objects.get_or_create(name=user_group_name)
-        if created:
-            for p in GROUP_PERMS.get(user_group_name):
-                permission = Permission.objects.get(codename=p)
-                user_group.permissions.add(permission)
-
-        new_user.groups.add(user_group)
-
-        user_serializer = self.serializer_class(new_user)
-        return Response({"user": user_serializer.data}, status=status.HTTP_201_CREATED)
+class AddUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
