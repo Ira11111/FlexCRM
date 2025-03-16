f5277ec1738af9c828b517aa4c32d9837f3f0a78
senya <zernovasenia@gmail.com>
2025-03-16 23:06:18 +0300

    add pagination for CustomersList

--- frontend/FlexCRM/src/components/crm/customers/CustomersList.tsx
+++ frontend/FlexCRM/src/components/crm/customers/CustomersList.tsx
@@ -4,24 +4,26 @@ import Loader from "../../Loader/Loader.tsx";
 import {useNavigate} from "react-router-dom";
 import getAll from '../../../fetchData.ts';
 import {ROLE} from "../../../constants.ts";
+import {usePagination} from "../../../context/PaginationContext.tsx";
+import Pagination from "../Pagination/Pagination.tsx";
 
 function CustomersList (){
     const role_permissions = localStorage.getItem(ROLE)=="Operators";
     const navigate = useNavigate()
     const [customers, setCustomers] = useState([]);
     const [loading, setLoading] = useState(false);
-
-
+    const {endpoint, setEndpoint, previous, updatePagination, next} = usePagination()
 
     useEffect(()=>{
         setLoading(true)
         const getAllCustomers = async ()=>{
-            const res = await getAll('/api/customers/');
+            const res = await getAll(endpoint);
             setCustomers(res.results);
             setLoading(false);
+            updatePagination(res);
         }
         getAllCustomers()
-    }, [])
+    }, [endpoint])
 
     return <div className='wrapper'>
         {loading && <Loader />}
@@ -32,10 +34,7 @@ function CustomersList (){
         <div className='cards-container'>
             {customers.map((cur, index) => {return <CustomerCard role_permissions={role_permissions}  customer={cur} key={index} index={index}/>})}
         </div>
-        <div className='pagination__buttons'>
-            <button className='button'>Назад</button>
-            <button className='button'>Вперед</button>
-        </div>
+        <Pagination previous={previous} next={next} setEndpoint={setEndpoint}/>
     </div>
 
 }
