48a6e0a11ae2d079629899afd1dab3e1995cc892
Ira11111 <mirina-0208@yandex.ru>
2025-03-19 19:39:04 +0300

    customer structure update

--- backend/FlexCRM/crmapp/serializers.py
+++ backend/FlexCRM/crmapp/serializers.py
@@ -1,7 +1,20 @@
+from drf_spectacular.utils import extend_schema_field
 from rest_framework.relations import PrimaryKeyRelatedField
-from rest_framework.serializers import ModelSerializer, IntegerField
+from rest_framework.serializers import ModelSerializer, IntegerField, SerializerMethodField
 from .models import Lead, Customer, Add, Contract, Product
-from django.db.models import F
+
+
+class AddListSerializer(ModelSerializer):
+    class Meta:
+        model = Add
+        fields = ["id", "name"]
+
+
+class AddSerializer(ModelSerializer):
+    class Meta:
+        model = Add
+        fields = ("id", "name", "budget", "customers_count",
+                  "profit", "product")
 
 
 class LeadSerializer(ModelSerializer):
@@ -13,13 +26,18 @@ class LeadSerializer(ModelSerializer):
         }
 
 
-class CustomerSerializer(ModelSerializer):
-    contracts = IntegerField(read_only=True, source='contract_count')
+class CustomerListSerializer(ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ["id", "name"]
+
+
+class CustomerCreateSerializer(ModelSerializer):
     adds = PrimaryKeyRelatedField(queryset=Add.objects.only("pk", "customers_count").all(), many=True)
 
     class Meta:
         model = Customer
-        fields = ["name", "lead", "is_active", "adds", "contracts"]
+        fields = ["id", "name", "description", "lead", "adds"]
 
     def create(self, validated_data):
         adds_data = validated_data.get("adds", [])
@@ -27,15 +45,44 @@ class CustomerSerializer(ModelSerializer):
             for add in adds_data:
                 add.customers_count += 1
             Add.objects.bulk_update(adds_data, ["customers_count"])
-
         return super().create(validated_data)
 
+    def update(self, instance, validated_data):
+        new_data = validated_data.get("adds", [])
+        print(new_data)
 
-class AddSerializer(ModelSerializer):
+        cur_data = instance.adds.all()
+        remove_adds = list(set(cur_data).difference(set(new_data)))
+        additional_adds = list(set(new_data).difference(set(cur_data)))
+
+        for add in remove_adds:
+            add.customers_count -= 1
+
+        for add in additional_adds:
+            add.customers_count += 1
+
+        Add.objects.bulk_update(additional_adds, ["customers_count"])
+        Add.objects.bulk_update(remove_adds, ["customers_count"])
+
+        return super().update(instance, validated_data)
+
+
+class CustomerDetailSerializer(ModelSerializer):
+    contracts_amount = IntegerField(read_only=True, source='contract_count')
+    lead_info = SerializerMethodField("serialize_lead")
+    adds_info = SerializerMethodField("serialize_add")
+
+    @extend_schema_field(LeadSerializer)
+    def serialize_lead(self, customer):
+        return LeadSerializer(customer.lead).data
+
+    @extend_schema_field(AddListSerializer)
+    def serialize_add(self, customer):
+        return AddListSerializer(customer.adds.only("id", "name").all(), many=True).data
+
     class Meta:
-        model = Add
-        fields = ("id", "name", "budget", "customers_count",
-                  "profit", "product")
+        model = Customer
+        fields = ["name", "description", "lead_info", "is_active", "adds_info", "contracts_amount"]
 
 
 class ContractSerializer(ModelSerializer):
@@ -45,8 +92,8 @@ class ContractSerializer(ModelSerializer):
 
 
 class ProductSerializer(ModelSerializer):
-    contracts = IntegerField(read_only=True, source='contract_count')
+    contracts_amount = IntegerField(read_only=True, source='contract_count')
 
     class Meta:
         model = Product
-        fields = ["id", "name", "description", "cost", "is_active", "contracts"]
+        fields = ["id", "name", "description", "cost", "is_active", "contracts_amount"]
--- backend/FlexCRM/crmapp/views.py
+++ backend/FlexCRM/crmapp/views.py
@@ -14,25 +14,12 @@ from .models import Lead, Contract, Add, Product, Customer
 from .serializers import (
     LeadSerializer,
     ContractSerializer,
-    CustomerSerializer,
+    CustomerListSerializer,
     AddSerializer,
     ProductSerializer,
+    CustomerDetailSerializer,
+    CustomerCreateSerializer,
 )
-
-
-@receiver(post_save, sender=Contract)
-def make_new_file_path(sender, instance, created, **kwargs):
-    if created and instance.contr_file:
-        old_path = instance.contr_file.name
-        new_path = "contracts/contract_{pk}/{filename}".format(
-            pk=instance.pk,
-            filename=os.path.basename(old_path),
-        )
-        default_storage.save(new_path, default_storage.open(old_path))
-        default_storage.delete(old_path)
-
-        instance.contr_file.name = new_path
-        instance.save(update_fields=['contr_file'])
 
 
 class LeadViewSet(ModelViewSet):
@@ -40,30 +27,31 @@ class LeadViewSet(ModelViewSet):
     serializer_class = LeadSerializer
 
 
-class ContractViewSet(ModelViewSet):
-    queryset = Contract.objects.all()
-    serializer_class = ContractSerializer
-    filter_backends = [
-        SearchFilter,
-        OrderingFilter,
-        DjangoFilterBackend
-    ]
-    ordering_fields = "name", "start_date", "end_date", "cost"
-    search_fields = "name",
-
-
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
-
     filter_backends = [
         SearchFilter,
         OrderingFilter,
         DjangoFilterBackend
     ]
     ordering_fields = "name",
-    search_fields = "name",
+    search_fields = "name", "description"
     filterset_fields = "is_active",
+
+    def get_queryset(self):
+        if self.action in ('list', 'destroy'):
+            return Customer.objects.only("id", "name").all()
+
+        if self.action in ('update', 'partial_update', 'retrieve'):
+            return Customer.objects.select_related('lead').prefetch_related('adds').all()
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'destroy'):
+            return CustomerListSerializer
+        elif self.action in ('update', 'partial_update', 'create'):
+            return CustomerCreateSerializer
+        elif self.action == 'retrieve':
+            return CustomerDetailSerializer
 
 
 class ProductSetView(ModelViewSet):
@@ -93,6 +81,33 @@ class AddSetView(ModelViewSet):
     search_fields = "name",
 
 
+@receiver(post_save, sender=Contract)
+def make_new_file_path(sender, instance, created, **kwargs):
+    if created and instance.contr_file:
+        old_path = instance.contr_file.name
+        new_path = "contracts/contract_{pk}/{filename}".format(
+            pk=instance.pk,
+            filename=os.path.basename(old_path),
+        )
+        default_storage.save(new_path, default_storage.open(old_path))
+        default_storage.delete(old_path)
+
+        instance.contr_file.name = new_path
+        instance.save(update_fields=['contr_file'])
+
+
+class ContractViewSet(ModelViewSet):
+    queryset = Contract.objects.all()
+    serializer_class = ContractSerializer
+    filter_backends = [
+        SearchFilter,
+        OrderingFilter,
+        DjangoFilterBackend
+    ]
+    ordering_fields = "name", "start_date", "end_date", "cost"
+    search_fields = "name",
+
+
 class StatisticsView(APIView):
 
     def get(self, request: HttpRequest) -> Response:
@@ -107,7 +122,7 @@ class StatisticsView(APIView):
         statistics = {
             "adds_customers": AddSerializer(adds_customers, many=True).data,
             "adds_profit": AddSerializer(adds_profit, many=True).data,
-            "customers": CustomerSerializer(customers, many=True).data,
+            "customers": CustomerListSerializer(customers, many=True).data,
             "products": ProductSerializer(products, many=True).data,
         }
         return Response(statistics)
