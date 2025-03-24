237685dd9439cdd21549f7b0fcd236c9cfc1e40c
senya <zernovasenia@gmail.com>
2025-03-24 13:29:12 +0300

    update frontend conf

--- frontend/FlexCRM/vite.config.ts
+++ frontend/FlexCRM/vite.config.ts
@@ -3,14 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base:'/',
     plugins: [react()],
-    preview: {
+    server: {
+        host: '0.0.0.0',
         port: 5173,
         strictPort: true,
     },
-    server: {
-       port: 5173,
-        strictPort: true,
-       }
+
 })
